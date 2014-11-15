b98feb87149aa1fb8244bd9db0106a9b6763d20e
JDH9 <jheimpel@live.com>
2014-11-15 06:04:57 +0000

    Finished two exercises for functions

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -29,33 +29,3 @@ module.exports.canIGet = function(item, money){
 
 
 
-// CHECK THIS WITH MATTHEW
-// module.exports.checkLock = function(firstNum, secondNum, thirdNum, fourthNum){
-//  if ((firstNum === (3 || 5 || 7)) && (secondNum === 2) && (thirdNum === ())) {
-
-// Planning individual condition statements
-//(first === 3 || first === 5 || first === 7)
-//(second === 2)
-//(third >= 5 && third <= 100)
-//(fourth < 9 || fourth > 20)
-
-/* 
-} else if (item === "MacBook Pro" && money >= 1299){
-    return true;
-    } else {
-      return false;
-    }
-  } else if (item === "Mac Pro" && money >= 2499){
-    return true;
-  } else {
-      return false;
-    }
-  } else if (item = "Apple Sticker" && money >= 1){
-      return true;
-  } else{
-      return false;
-  }
-} else {
-  return false;
-
-  */
