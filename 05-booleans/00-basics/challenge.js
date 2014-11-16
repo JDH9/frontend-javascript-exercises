bea9c1073f53a2ff6d275ea1d32ff75c1cea6539
JDH9 <jheimpel@live.com>
2014-11-16 17:35:55 +0000

    finished exercises for boolean basics

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,40 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-
+  return stringOne === stringTwo;
 };
 
 module.exports.notEqual = function(one, two) {
-
+  return one !== two;
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-
+  return lower < middle && middle < upper;
 };
 
 module.exports.outsideRanges = function(number) {
-
+  if(((number >= 10 && number <= 20) || (number > 42 && number <= 75) || (number > 1 && number < 6))){
+    return false;
+  }else{
+    return true;
+  }
 };
+
+
+
+//  return !((number >= 10 && number <= 20) && (number > 42 && number <= 75) && (number > 1 && number < 6));
+//};
+
+// 4 < middle && middle < 6
+
+// not between 10 and 20, it also can't be 10 or 20
+  //(number !>= 10 && number !<= 20) 
+// not between 42 and 75, it also can't be 75. 42 is allowed
+  //(number !> 42 && number <= 75)
+// not between 1 and 6. 1 and 6 are allowed
+  //(number !> 1 && number !< 6)
 
 module.exports.nameAndPrice = function(name, price) {
-
+  return (name == "NYTimes" || name == "LATimes") && (price >= 1);
 };
+
+
+
